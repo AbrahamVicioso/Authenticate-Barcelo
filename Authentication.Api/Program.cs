419ebb9dfde148df2dcd4f33035f84cb7925bf79
AVICIOSO <AVICIOSO@COMP-AVICIOSO>
2025-10-22 14:38:10 -0400

    Add project files.

---
+++ Authentication.Api/Program.cs
@@ -0,0 +1,46 @@
+﻿using Authentication.Domain.Entities;
+using Authentication.Persistence;
+using Authentication.Persistence.Data;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddPersistenceLayer();
+
+builder.Services
+    .AddAuthentication()
+    .AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services
+    .AddIdentityCore<User>(options => options.SignIn.RequireConfirmedEmail = false)
+    .AddEntityFrameworkStores<AuthenticationDbContext>()
+    .AddApiEndpoints();
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapIdentityApi<User>();
+
+app.MapControllers();
+
+app.Run();
